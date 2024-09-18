9ab467d5951a4a9c76770b0c1b4ed28440474363
Oleksandr Romashko <oromashk@c2r1s3.42prague.com>
2024-09-18 21:27:33 +0200

    Added libft project to 42cursus, written some functions, header file and Makefile there. Removed Evaluations folder from C_Piscine

---
+++ 42cursus/libft/libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oromashk <oromashk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 20:04:17 by oromashk          #+#    #+#             */
+/*   Updated: 2024/09/18 21:22:06 by oromashk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+size_t				ft_strlen(const char *s);
+
+#endif
