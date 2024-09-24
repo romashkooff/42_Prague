ac4ad18e80105dad92ef6866cc84d5736e6bae66
Oleksandr Romashko <oromashk@c2r1s3.42prague.com>
2024-09-24 12:59:07 +0200

    Completed mandatory part of the project with this new fuctions. Also modified Makefile and header file

--- 42cursus/libft/libft.h
+++ 42cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: oromashk <oromashk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 20:04:17 by oromashk          #+#    #+#             */
-/*   Updated: 2024/09/23 12:20:00 by oromashk         ###   ########.fr       */
+/*   Updated: 2024/09/24 12:31:49 by oromashk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,11 @@ char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
 char				**ft_split(char const *s, char c);
 char				*ft_itoa(int n);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void				ft_striteri(char *s, void (*f)(unsigned int, char *));
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
 
 #endif
